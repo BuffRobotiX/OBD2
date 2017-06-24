9c374486945d1f2abbf73d37e6755ef47ef98413
David F. Buff <buff.robotix@yahoo.com>
2017-06-24 00:31:41 -0700

    The rest.

--- OBD2.js
+++ OBD2.js
@@ -1,22 +1,117 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+db = new Mongo.Collection('db')
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+
+  Meteor.subscribe('db')
+
+  Session.set("template", "hello")
+
+  Meteor.startup(function () {
+        setInterval(function () {
+          Session.set("time", new Date());
+        }, 1000);
+
+        setInterval(function () {
+          if (db.find({name: "swipe"}).fetch().length > 0) {
+            if (Session.get("template") == "hello")
+              Session.set("template", "goodbye")
+            else
+              Session.set("template", "hello")
+            db.remove({name: "swipe"})
+          }
+      }, 10);
+    });
+
+  Template.main.helpers({
+    template: function () {
+      return Session.get("template")
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.hello.helpers({
+    rpm: function () {
+      return db.find({name: "rpm"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value
+    },
+    rpmPct: function () {
+      return db.find({name: "rpm"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value / 7000.0
+    },
+    vss: function () {
+      return db.find({name: "vss"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value
+    },
+    temp: function () {
+      return parseFloat(db.find({name: "temp"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value) * 9.0 / 5.0 + 32
+    },
+    load_pct: function () {
+      return db.find({name: "load_pct"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value
+    },
+    time: function () {
+      t = Session.get("time")
+      var hour = t.getHours()
+      if (hour > 12)
+        hour -= 12
+      if (t.getMinutes() < 10)
+        return hour + ":0" + t.getMinutes()
+      else
+        return hour + ":" + t.getMinutes()
+    },
+    seconds: function () {
+      t = Session.get("time")
+      if (t.getSeconds() < 10)
+        return "0" + t.getSeconds()
+      else
+        return t.getSeconds()
+    }
+  });
+
+  Template.goodbye.helpers({
+    rpm: function () {
+      return db.find({name: "rpm"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value
+    },
+    rpmPct: function () {
+      return db.find({name: "rpm"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value / 7000.0
+    },
+    vss: function () {
+      return db.find({name: "vss"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value
+    },
+    temp: function () {
+      return parseFloat(db.find({name: "temp"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value) * 9.0 / 5.0 + 32
+    },
+    load_pct: function () {
+      return db.find({name: "load_pct"}, {sort: {timestamp: -1}, limit: 1}).fetch()[0].value
+    },
+    time: function () {
+      t = Session.get("time")
+      var hour = t.getHours()
+      if (hour > 12)
+        hour -= 12
+      if (t.getMinutes() < 10)
+        return hour + ":0" + t.getMinutes()
+      else
+        return hour + ":" + t.getMinutes()
+    },
+    seconds: function () {
+      t = Session.get("time")
+      if (t.getSeconds() < 10)
+        return "0" + t.getSeconds()
+      else
+        return t.getSeconds()
     }
   });
 }
 
 if (Meteor.isServer) {
+
+  Meteor.publish('db', function() {
+    return db.find({}, {sort:{timestamp: -1}, limit: 4});
+});
+
+  Meteor.methods({
+    getSwipe: function (text) {
+      db.remove({name: "swipe"})
+      db.insert({name: "swipe"})
+    }
+  });
+
   Meteor.startup(function () {
     // code to run on server at startup
     var obdReader = Meteor.npmRequire('bluetooth-obd')
@@ -26,16 +121,16 @@ if (Meteor.isServer) {
       this.addPoller("rpm");
       this.addPoller("temp");
       this.addPoller("load_pct");
-      this.addPoller("map");
-      this.addPoller("frp");
    
       this.startPolling(1000);
     });
 
-    obd.on('dataReceived', function (data) {
-      console.log(data)
-      dataReceivedMarker = data
-    });
+    obd.on('dataReceived', Meteor.bindEnvironment(function (data, error) {
+        console.log(data)
+        if (data.value != "NO DATA")
+          db.insert({name: data.name, value: data.value, timestamp: new Date()})
+      })
+    )
 
     obd.autoconnect('OBDII')
   });
